f8ae38dc7a543a464ac5ba27fc74c7d28c696055
Carlos <carlosejimenezh95@gmail.com>
2022-03-17 09:19:40 -0600

    agregar clase de txt amarillo en span

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
         <div class="w3-row w3-center contenido contenido-small">
             <div class="w3-half mitadizq">
                 <h2>Nosotros</h2>
-                <span>prueba</span>
+                <span class="txtamarillo">prueba</span> <br>
                 omos una empresa dedicada a la planeación y ejecución de proyectos constructivos. Fundada en la ciudad de Ensenada, Baja California en el año 2017. <br>
                 Destaca nuestra honestidad, ingenio y la calidad en nuestros servicios. Si deseas invertir en una propiedad, hablemos de construir.
             </div>
             <div class="w3-half mitadder">
                 <h2>Misión</h2>
-                <span>prueba</span>
+                <span class="txtamarillo">prueba</span> <br>
                 Brindar soluciones creativas y facilitar los procesos constructivos para asegurar el éxito de los proyectos de nuestros clientes.
             </div>
         </div>
