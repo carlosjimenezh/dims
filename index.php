8dd0087fe75282ae2281548abf71472423d43b07
Carlos <carlosejimenezh95@gmail.com>
2022-03-22 09:22:09 -0600

    agregar plantilla para formulario php

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
     <link rel="stylesheet" href="swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="w3pro.css">
     <link rel="stylesheet" href="estilo.css">
+    <script type="text/javascript">
+        function verifica()
+        {
+            if(document.contacto.nombre.value.trim()==""  || document.contacto.email.value.trim()=="" || document.contacto.producto.value.trim()=="")
+            {
+                alert("Es necesario escribir tu nombre, e-mail y el diseño de producto");     
+            }
+            else
+            {
+                var s = document.contacto.email.value;
+                var filter=/^[A-Za-z][A-Za-z0-9_.ñÑ-]*@[A-Za-z0-9_ñÑ-]+\.[A-Za-z0-9_.ñÑ-]+[A-za-z]$/;
+                if(filter.test(s))
+                {            
+                    document.contacto.submit();
+                }
+                else
+                {
+                    alert("Por favor, escribe una dirección de e-mail válida");
+                }
+            }
+        }
+	</script>
     <title>Dims</title>
 </head>
 <body> 
@@ -130,6 +152,65 @@
     </section>
 
     <section id="contacto" class="contenido contenido-small">
+        <a name="scontacto"></a>
+    	<?php
+	    if(isset($_GET['a']) && $_GET['a']==2 && isset($_POST['email']) && isset($_POST['nombre']) 
+        && isset($_POST['producto']) && trim($_POST['email'])!="" && trim($_POST['nombre'])!="" 
+        && trim($_POST['producto'])!=""){
+
+			if(filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){	
+				$nombre=strip_tags($_POST['nombre']);
+				$from = $_POST['email'];
+				$email_from = $from;
+				$email_txt = "Este es un correo enviado desde el formulario web de SOLIDWORKS<br><br>
+                &middot; Nombre: ".$nombre."<br>
+                &middot; E-mail: ".$_POST['email']."<br>
+                &middot; Empresa: ".strip_tags($_POST['empresa'])."<br>
+                &middot; Teléfono: ".strip_tags($_POST['telefono'])."<br>
+				&middot; Diseño de producto: ".strip_tags($_POST['producto'])."<br><br>";
+				
+				if($_POST['capacitacion'] || $_POST['licencias']){
+					$email_txt.="INTERÉS:<br>";
+					if($_POST['capacitacion'])
+						$email_txt.="- Capacitación<br>";
+					if($_POST['licencias'])
+						$email_txt.="- Implementar licencias<br>";
+				}
+				
+                
+					$email_to= "carlosejimenezh95@gmail.com";
+					$email_subject = "contacto de ".$nombre;
+				
+				$headers = "From: ".$email_from;
+				
+				$semi_rand = md5(time());
+				$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+                
+				$headers .= "\nMIME-Version: 1.0\n" . 
+							"Content-Type: multipart/mixed;\n" . 
+							" boundary=\"{$mime_boundary}\"";  
+				$email_message .= $email_txt. "\n\n" . 
+								"--{$mime_boundary}\n" . 
+								"Content-Type:text/html; charset=\"utf-8\"\n" . 
+							   "Content-Transfer-Encoding: 7bit\n\n" . 
+				$email_txt . "\n\n";  
+				
+				$data = chunk_split(base64_encode($data)); 
+				
+				$ok = @mail($email_to, $email_subject, $email_message, $headers); 
+				if($ok)
+					echo "<br><br><div class=\"aviso txtazul\">Gracias por escribirnos,<br>
+                    nos pondremos en contacto lo antes posible.</div><br><br><br>";
+				else
+					echo "<br><br><div class=\"aviso txtazul\">Hubo un error al enviar su mensaje, 
+                    inténtelo de nuevo o escríbanos a <span style=\"font-family:arial, 
+                    sans-serif\">ventas@.com</span></div><br><br><br>";
+				
+			}
+			else
+				echo "<br><br><div class=\"aviso\">E-mail no válido.</div><br><br><br>";
+	    }
+	    else{?>
         <h2 class="w3-center">Contacto</h2>
         <form action="" class="w3-row">
             <div class="w3-half mitadizq">
@@ -149,6 +230,8 @@
                 <textarea name="mensaje" id="" cols="30" rows="7"></textarea>
             </div>
         </form>
+        <?php
+	    }?>
     </section>
     
 
